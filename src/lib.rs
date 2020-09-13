2005b89de9ac6dfe133ad98e4e6fe03703d249da
Michele Lacchia <michelelacchia@gmail.com>
2020-09-13 12:17:12 +0200

    chore: add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+//! The parser handles comments, strings and the `export` syntax automatically.
+//!
+//! # Example
+//! ```
+//! use dotenv_parser::parse_dotenv;
+//!
+//! fn main() {
+//!     let source = r#"
+//!         ## main comment
+//!         ENV_FOR_HYDRO='testing 2' # another one here
+//!         export USER_ID=5gpPN5rcv5G41U_S
+//!         API_TOKEN=30af563ccc668bc8ced9e24e  # relax! these values are fake
+//!         APP_SITE_URL=https://my.example.com
+//!     "#;
+//!
+//!     let map = vec![
+//!         ("ENV_FOR_HYDRO", "testing 2"),
+//!         ("USER_ID", "5gpPN5rcv5G41U_S"),
+//!         ("API_TOKEN", "30af563ccc668bc8ced9e24e"),
+//!         ("APP_SITE_URL", "https://my.example.com"),
+//!     ]
+//!     .into_iter()
+//!     .map(|(a, b)| (a.into(), b.into()))
+//!     .collect();
+//!
+//!     let res = parse_dotenv(source).unwrap();
+//!     assert_eq!(res, map);
+//! }
+
+#![deny(missing_docs)]
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
--- src/parser.rs
+++ src/parser.rs
@@ -153,6 +153,8 @@ mod tests {
     #[test]
     fn complete_dotenv() {
         let source = r#"
+            # main comment
+
             ENV_FOR_HYDRO='testing 2' # another one here
             export USER_ID=5gpPN5rcv5G41U_S
             API_TOKEN=30af563ccc668bc8ced9e24e  # relax! these values are fake
